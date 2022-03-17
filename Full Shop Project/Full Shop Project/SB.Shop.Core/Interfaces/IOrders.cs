0092d5e5352d87a7a8d924d564782e6303770f5b
ori tikozki <oritikozki@gmail.com>
2022-03-18 00:06:50 +0200

    adding interfaces

---
+++ Full Shop Project/Full Shop Project/SB.Shop.Core/Interfaces/IOrders.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Full_Shop_Project.SB.Shop.Core.Interfaces
+{
+    internal interface IOrders
+    {
+        public int GetOrderID();
+        public DateTime GetOrderDate(int orderId);
+    }
+}
--- Full Shop Project/Full Shop Project/SB.Shop.Core/Interfaces/IUsers.cs
+++ Full Shop Project/Full Shop Project/SB.Shop.Core/Interfaces/IUsers.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace Full_Shop_Project.SB.Shop.Core.Interfaces
 {
-    internal interface Interface1
+    internal interface IUsers
     {
+        public static User CreateClientInstance(string FirstName, string LastName, int PhoneNumber, string Email, string UserName, string Password)
+        {
+            User newClient = new User();
+
+            newClient.FirstName = FirstName;
+            newClient.LastName = LastName;
+            newClient.PhoneNumber = PhoneNumber;
+            newClient.Email = Email;
+            newClient.UserName = UserName;
+            newClient.Password = Password;
+
+            return newClient;
+        }
     }
 }
